1741b3c91d3464918d8fe1f93c6431ebc9195773
Steve Miner <steveminer@gmail.com>
2015-03-05 11:48:42 -0500

    first tests

--- src/miner/transmuters.clj
+++ src/miner/transmuters.clj
@@ -12,7 +12,10 @@
   ([coll] (empty coll)))
   
 
-;; I think (take-nth 0) should always be the empty list
+;; I think (take-nth 0) should always be the empty list.
+;; This is a change from the way it works in Clojure 1.6.
+;; Clojure 1.7.0-alpha5 throws on (take-nth 0), mine gives a transducer that will return ().
+;; See also http://dev.clojure.org/jira/browse/CLJ-1665
 (defn my-take-nth
   "Returns a lazy seq of every nth item in coll.  Returns a stateful
   transducer when no collection is provided.  N=0 returns empty list."
@@ -73,3 +76,47 @@
    (if (zero? n)
      coll
      (lazy-drop-nth n coll))))
+
+
+;; collection version by Frank on mailing list
+;; SEM added transducer version, adapted from partition-by
+(defn partition-when
+   "Applies f to each value in coll, starting a new partition each time f returns a
+   true value.  Returns a lazy seq of partitions.  Returns a stateful
+   transducer when no collection is provided."
+  {:added "1.X"
+   :static true}
+  ([f]
+   (fn [rf]
+     (let [a (java.util.ArrayList.)]
+       (fn
+         ([] (rf))
+         ([result]
+          (let [result (if (.isEmpty a)
+                         result
+                         (let [v (vec (.toArray a))]
+                           ;;clear first!
+                           (.clear a)
+                           (unreduced (rf result v))))]
+            (rf result)))
+         ([result input]
+          (let [val (f input)]
+            (if val
+              (let [v (vec (.toArray a))]
+                (.clear a)
+                (let [ret (rf result v)]
+                  (when-not (reduced? ret)
+                    (.add a input))
+                  ret))
+              (do
+                (.add a input)
+                result))))))))
+
+  ([f coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (let [fst (first s)
+           run (cons fst (take-while #(not (f %)) (next s)))]
+       (cons run (partition-when f (seq (drop (count run) s)))))))))
+
+
--- test/miner/test_transmuters.clj
+++ test/miner/test_transmuters.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [miner.transmuters :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest range100
+  (let [coll (range 100)]
+    (is (= (clojure.core/take-nth 3 coll)
+           (my-take-nth 3 coll)
+           (sequence (my-take-nth 3) coll)
+           (transduce (my-take-nth 3) conj [] coll)))
+    (is (empty? (sequence (my-empty) coll)))
+    (is (empty? (my-empty coll)))))
+
+    
+
+  
