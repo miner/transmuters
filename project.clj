c76a869c877f71027264a114a486c73a0309da91
Steve Miner <steveminer@gmail.com>
2025-08-26 17:17:32 -0400

    libs

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.velisco/transmuters "0.4.2-SNAPSHOT"
+(defproject com.velisco/transmuters "0.4.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/miner/transmuters"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [criterium "0.4.6"]])
 
